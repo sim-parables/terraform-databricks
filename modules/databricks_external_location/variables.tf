650afaed917f2c7cd0cadb76547530acb57ed7a5
retrhinkr-hub <jason.grein@gmail.com>
2024-06-12 20:02:37 -0400

    Removed aws specific variables

--- modules/databricks_external_location/variables.tf
+++ modules/databricks_external_location/variables.tf
@@ -3,11 +3,6 @@
 ## These variables are expected to be passed in by the operator
 ## ---------------------------------------------------------------------------------------------------------------------
 
-variable "aws_external_bucket_name" {
-  type        = string
-  description = "AWS S3 External Locations Bucket for Databricks"
-}
-
 variable "databricks_storage_credential_id" {
   type        = string
   description = "Databricks Storage Credential"
