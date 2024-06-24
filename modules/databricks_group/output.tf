7e2a8ce2b1a3fa23902b9c56e814f174c439622c
retrhinkr-hub <jason.grein@gmail.com>
2024-06-24 17:36:25 -0400

    Added new outputs to databricks_group module

--- modules/databricks_group/output.tf
+++ modules/databricks_group/output.tf
@@ -2,3 +2,13 @@ output "databricks_group_name" {
   description = "Databricks Group/Team Name"
   value       = databricks_group.this.display_name
 }
+
+output "databricks_group_id" {
+  description = "Databricks Group ID"
+  value = databricks_group.this.id
+}
+
+output "databricks_group_external_id" {
+  description = "Databricks Group External ID"
+  value = databricks_group.this.external_id
+}
